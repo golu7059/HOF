f4f4f935f9d83ac9030063976a673f6c5c539a3b
GOLU KUMAR <golukumarjaisidih@gmail.com>
2023-09-02 15:11:46 +0530

    Add files via upload

---
+++ linkedinvalidator.js
@@ -0,0 +1,25 @@
+// 6. LinkedIn Profile URL Validator.
+// As a developer, you want to create a program that validates LinkedIn profile URLs to ensure that they are
+// formatted correctly and contain only valid characters. Valid LinkedIn profile URLs should start with https://
+// www.linkedin.com/in/ followed by a combination of one or more letters, digits, underscores, or hyphens, and
+// end with a letter or digit. The length of the profile ID should be between 5 and 30 characters.
+// The program should use a regular expression to match valid LinkedIn profile URLs, and should not match URLs
+// that do not follow this format or contain invalid characters. The program should provide clear output messages
+// indicating whether each input is a valid LinkedIn profile URL or not.
+
+
+
+
+function validateURL(url) {
+    const regex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]{5,30}[a-zA-Z0-9]$/;
+    const isValid = regex.test(url);
+  
+    if (isValid) {
+      console.log("The URL is valid.");
+    } else {
+      console.log("The URL is not valid.");
+    }
+  }
+
+
+  validateURL('https://www.linkedin.com/in/johndoe');
